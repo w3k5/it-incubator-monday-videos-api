f9663834c1eda00f4d46577e0049ad8b996e57d0
Vladislav Marchenko <w3k5@yandex.ru>
2022-08-16 16:52:23 +0300

    Dropped mock data

--- src/repositories/videos-repository.ts
+++ src/repositories/videos-repository.ts
@@ -3,63 +3,7 @@ import { ResolutionEnum } from '../enums';
 import { videoGenerator } from '../services/video-generator';
 import { CreateVideoType, UpdateVideoType } from '@types';
 
-const MOCK_VIDEOS: VideoInterface[] = [
-	{
-		id: 1,
-		title: 'About JS - 01',
-		author: 'it-incubator.eu',
-		canBeDownloaded: true,
-		minAgeRestriction: 18,
-		createdAt: '2022-08-16T02:20:29.750Z',
-		publicationDate: '2022-08-16T02:20:29.750Z',
-		availableResolutions: [ResolutionEnum.P144, ResolutionEnum.P240],
-	},
-	{
-		id: 2,
-		title: 'About JS - 02',
-		author: 'it-incubator.eu',
-		canBeDownloaded: false,
-		minAgeRestriction: 6,
-		createdAt: '2022-08-16T02:20:29.750Z',
-		publicationDate: '2022-08-16T02:20:29.750Z',
-		availableResolutions: [
-			ResolutionEnum.P144,
-			ResolutionEnum.P240,
-			ResolutionEnum.P360,
-			ResolutionEnum.P480,
-		],
-	},
-	{
-		id: 3,
-		title: 'About JS - 03',
-		author: 'it-incubator.eu',
-		canBeDownloaded: true,
-		minAgeRestriction: 12,
-		createdAt: '2022-08-16T02:20:29.750Z',
-		publicationDate: '2022-08-16T02:20:29.750Z',
-		availableResolutions: [
-			ResolutionEnum.P144,
-			ResolutionEnum.P240,
-			ResolutionEnum.P720,
-			ResolutionEnum.P1080,
-		],
-	},
-	{
-		id: 4,
-		title: 'About JS - 04',
-		author: 'it-incubator.eu',
-		canBeDownloaded: true,
-		minAgeRestriction: 0,
-		createdAt: '2022-08-16T02:20:29.750Z',
-		publicationDate: '2022-08-16T02:20:29.750Z',
-		availableResolutions: [
-			ResolutionEnum.P144,
-			ResolutionEnum.P240,
-			ResolutionEnum.P1080,
-			ResolutionEnum.P2160,
-		],
-	},
-];
+const MOCK_VIDEOS: VideoInterface[] = [];
 
 /**
  * Repository for working with Videos
