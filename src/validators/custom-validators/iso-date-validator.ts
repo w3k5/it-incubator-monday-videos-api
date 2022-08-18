c93f77938ff642ef27c7e467df398e8d76d65c75
Vladislav Marchenko <w3k5@yandex.ru>
2022-08-18 19:51:07 +0300

    iso date validator

--- src/validators/custom-validators/iso-date-validator.ts
+++ src/validators/custom-validators/iso-date-validator.ts
@@ -1,6 +1,9 @@
 import { CustomValidator } from 'express-validator';
 
-export const customIsoDateValidator: CustomValidator = (value: any) => {
+export const customIsoDateValidator: CustomValidator = (value: string) => {
+	if (value.length !== 12) {
+		throw new Error('Invalid date format');
+	}
 	const parsedDate = new Date(value).valueOf();
 	if (parsedDate < 0 || isNaN(parsedDate)) {
 		throw new Error('Date must be in a Date format!');
--- src/validators/video-validators/field-validators/publication-date-validations.ts
+++ src/validators/video-validators/field-validators/publication-date-validations.ts
@@ -5,7 +5,11 @@ import { customIsoDateValidator } from '../../custom-validators/iso-date-validat
  * Publication date filed validations
  */
 const publicationDateValidation = body('publicationDate')
+	.trim()
+	.isString()
 	.custom(customIsoDateValidator)
+	.isISO8601()
+	.toDate()
 	.optional({ nullable: true });
 
 export const publicationDateValidators = [publicationDateValidation];
